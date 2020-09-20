a758d0083818256b51f7d678675e684c9a7d91d6
afifuddin <afifbasya@gmail.com>
2020-09-20 20:02:57 +0700

    List Category

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ export default class App extends Component {
   
     this.state = {
        menus: [],
+       categoriYangDipilih: 'Makanan'
     }
   }
 
   componentDidMount() {
     axios
-      .get(API_URL+"products")
+      .get(API_URL+"products?category.nama="+this.state.categoriYangDipilih)
       .then(res => {
         const menus = res.data;
         this.setState({ menus });
@@ -24,16 +25,34 @@ export default class App extends Component {
         console.log("Error yaa " , error);
       })
   }
+
+  changeCategory = (value) => {
+    this.setState({
+      categoriYangDipilih: value,
+      menus: []
+    })
+
+    axios
+      .get(API_URL+"products?category.nama="+value)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log("Error yaa " , error);
+      })
+
+  }
   
   render() {
-    const { menus } = this.state
+    const { menus, categoriYangDipilih } = this.state
     return (
       <div className="App">
       <NavbarComponent />
       <div className="mt-3">
         <Container fluid>
           <Row>
-            <ListCategories />
+            <ListCategories changeCategory={this.changeCategory} categoriYangDipilih={categoriYangDipilih}/>
             <Col>
               <h4>
                 <strong>Daftar Produk</strong>
--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -1,13 +1,70 @@
-import React, { Component } from 'react'
-import { Col } from 'react-bootstrap'
+import React, { Component } from "react";
+import { Col, ListGroup } from "react-bootstrap";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faCoffee,
+  faCheese,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Icon = ({ nama }) => {
+  if (nama === "Makanan")
+    return <FontAwesomeIcon icon={faUtensils} className="mr-2" />;
+  if (nama === "Minuman") return <FontAwesomeIcon icon={faCoffee} />;
+  if (nama === "Cemilan")
+    return <FontAwesomeIcon icon={faCheese} className="mr-2" />;
+
+  return <FontAwesomeIcon icon={faUtensils} className="mr-2" />;
+};
 
 export default class ListCategories extends Component {
-    render() {
-        return (
-            <Col md={2} mt="2">
-                <h4><strong>Daftar Kategori</strong></h4>
-                <hr />
-            </Col>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "categories")
+      .then((res) => {
+        const categories = res.data;
+        this.setState({ categories });
+      })
+      .catch((error) => {
+        console.log("Error yaa ", error);
+      });
+  }
+
+  render() {
+    const { categories } = this.state;
+    const { changeCategory, categoriYangDipilih } = this.props;
+    return (
+      <Col md={2} mt="2">
+        <h4>
+          <strong>Daftar Kategori</strong>
+        </h4>
+        <hr />
+        <ListGroup>
+          {categories &&
+            categories.map((category) => (
+              <ListGroup.Item
+                key={category.id}
+                onClick={() => changeCategory(category.nama)}
+                className={categoriYangDipilih === category.nama && "category-aktif"}
+                style={{cursor: 'pointer'}}
+              >
+                <h5>
+                  <Icon nama={category.nama} /> {category.nama}
+                </h5>
+              </ListGroup.Item>
+            ))}
+        </ListGroup>
+      </Col>
+    );
+  }
 }
