3ee52d64d2a67d1a53910497c181f98a819199d5
afifuddin <afifbasya@gmail.com>
2020-09-20 20:58:43 +0700

    Membuat Keranjang Pemesanan

--- src/components/Hasil.js
+++ src/components/Hasil.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
-import { Col } from "react-bootstrap";
+import { Badge, Col, ListGroup, Row } from "react-bootstrap";
+import { numberWithCommas } from "../utils/utils";
 
 export default class Hasil extends Component {
   render() {
+    const { keranjangs } = this.props;
     return (
       <Col md={3} mt="2">
         <h4>
           <strong>Hasil</strong>
         </h4>
         <hr />
+        {keranjangs.length !== 0 && (
+          <ListGroup variant="flush">
+            {keranjangs.map((menuKeranjang) =>(
+               <ListGroup.Item>
+                 <Row>
+                   <Col xs={2}>
+                      <h4>
+                        <Badge pill variant="success">
+                          {menuKeranjang.jumlah}
+                        </Badge>
+                      </h4>
+                   </Col>
+                   <Col>
+                      <h5>{menuKeranjang.product.nama}</h5>
+                      <p>Rp. {numberWithCommas(menuKeranjang.product.harga)}</p>
+                   </Col>
+                   <Col>
+                      <strong className="float-right">Rp. {numberWithCommas(menuKeranjang.total_harga)}</strong>
+                   </Col>
+                 </Row>
+               </ListGroup.Item>
+            ))}
+           
+          </ListGroup>
+        )}
       </Col>
     );
   }
--- src/components/Menus.js
+++ src/components/Menus.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Col, Card } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
 
-const Menus = ({ menu }) => {
+const Menus = ({ menu, masukKeranjang }) => {
   return (
     <Col md={4} xs={6} className="mb-4">
-      <Card className="shadow">
+      <Card className="shadow" onClick={() => masukKeranjang(menu)}>
         <Card.Img
           variant="top"
           src={
