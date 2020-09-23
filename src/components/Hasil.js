191e90aafc94cdc17c2bbba04a57c549be115606
afifuddin <afifbasya@gmail.com>
2020-09-23 13:22:49 +0700

    Membuat Modal Edit Pemesanan

--- src/components/Hasil.js
+++ src/components/Hasil.js
@@ -1,9 +1,62 @@
 import React, { Component } from "react";
 import { Badge, Col, ListGroup, Row } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
+import ModalKeranjang from "./ModalKeranjang";
 import TotalBayar from "./TotalBayar";
 
 export default class Hasil extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModal: false,
+      keranjangDetail: false,
+      jumlah: 0,
+      keterangan: "",
+    };
+  }
+
+  handleShow = (menuKeranjang) => {
+    this.setState({
+      showModal: true,
+      keranjangDetail: menuKeranjang,
+      jumlah: menuKeranjang.jumlah,
+      keterangan: menuKeranjang.keterangan,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  tambah = () => {
+    this.setState({
+      jumlah: this.state.jumlah + 1,
+    });
+  };
+
+  kurang = () => {
+    if (this.state.jumlah !== 1) {
+      this.setState({
+        jumlah: this.state.jumlah - 1,
+      });
+    }
+  };
+
+  changeHandler = (event) => {
+    this.setState({
+      keterangan: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("Hai", this.state.keterangan);
+  }
+
   render() {
     const { keranjangs } = this.props;
     return (
@@ -14,31 +67,44 @@ export default class Hasil extends Component {
         <hr />
         {keranjangs.length !== 0 && (
           <ListGroup variant="flush">
-            {keranjangs.map((menuKeranjang) =>(
-               <ListGroup.Item key={menuKeranjang.id}>
-                 <Row>
-                   <Col xs={2}>
-                      <h4>
-                        <Badge pill variant="success">
-                          {menuKeranjang.jumlah}
-                        </Badge>
-                      </h4>
-                   </Col>
-                   <Col>
-                      <h5>{menuKeranjang.product.nama}</h5>
-                      <p>Rp. {numberWithCommas(menuKeranjang.product.harga)}</p>
-                   </Col>
-                   <Col>
-                      <strong className="float-right">Rp. {numberWithCommas(menuKeranjang.total_harga)}</strong>
-                   </Col>
-                 </Row>
-               </ListGroup.Item>
+            {keranjangs.map((menuKeranjang) => (
+              <ListGroup.Item
+                key={menuKeranjang.id}
+                onClick={() => this.handleShow(menuKeranjang)}
+              >
+                <Row>
+                  <Col xs={2}>
+                    <h4>
+                      <Badge pill variant="success">
+                        {menuKeranjang.jumlah}
+                      </Badge>
+                    </h4>
+                  </Col>
+                  <Col>
+                    <h5>{menuKeranjang.product.nama}</h5>
+                    <p>Rp. {numberWithCommas(menuKeranjang.product.harga)}</p>
+                  </Col>
+                  <Col>
+                    <strong className="float-right">
+                      Rp. {numberWithCommas(menuKeranjang.total_harga)}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
             ))}
-           
+
+            <ModalKeranjang
+              handleClose={this.handleClose}
+              {...this.state}
+              tambah={this.tambah}
+              kurang={this.kurang}
+              changeHandler={this.changeHandler}
+              handleSubmit={this.handleSubmit}
+            />
           </ListGroup>
         )}
 
-        <TotalBayar keranjangs={keranjangs} {...this.props}/>
+        <TotalBayar keranjangs={keranjangs} {...this.props} />
       </Col>
     );
   }
---
+++ src/components/ModalKeranjang.js
@@ -0,0 +1,95 @@
+import { faMinus, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import { numberWithCommas } from "../utils/utils";
+
+const ModalKeranjang = ({
+  showModal,
+  handleClose,
+  keranjangDetail,
+  jumlah,
+  keterangan,
+  tambah,
+  kurang,
+  changeHandler,
+  handleSubmit
+}) => {
+  if (keranjangDetail) {
+    return (
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {keranjangDetail.product.nama}{" "}
+            <strong>
+              (Rp. {numberWithCommas(keranjangDetail.product.harga)})
+            </strong>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Total Harga :</Form.Label>
+              <p>
+                <strong>
+                  Rp. {numberWithCommas(keranjangDetail.total_harga)}
+                </strong>
+              </p>
+            </Form.Group>
+
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Jumlah :</Form.Label>
+              <br />
+              <Button variant="primary" size="sm" className="mr-2" onClick={ () => tambah()}>
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
+              <strong>{jumlah}</strong>
+              <Button variant="primary" size="sm" className="ml-2" onClick={ () => kurang()}>
+                <FontAwesomeIcon icon={faMinus} />
+              </Button>
+            </Form.Group>
+
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Keterangan :</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows="3"
+                name="keterangan"
+                placeholder="Contoh : Pedes, Nasi Setengah"
+                value={keterangan}
+                onChange={(event) => changeHandler(event)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Simpan
+            </Button>   
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger">
+            <FontAwesomeIcon icon={faTrash} /> Hapus Pesanan
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  } else {
+    return (
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Kosong</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Kosong</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+};
+
+export default ModalKeranjang;
